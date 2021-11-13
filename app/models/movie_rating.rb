cd3b59a103311e1756e7663be4d9cb1f46ee31e6
ccfz <casparfz@gmail.com>
2021-11-13 22:32:50 +0100

    Add MovieRating.refresh function
    In order for the MovieRating materialized view to be update to date
    it needs a refresh function.
    
    This adds a refresh function to MovieRating

--- app/models/movie_rating.rb
+++ app/models/movie_rating.rb
@@ -2,4 +2,15 @@ class MovieRating < ApplicationRecord
   belongs_to :movie
   self.primary_key= :id
 
+  def self.refresh
+    Scenic.database.refresh_materialized_view(
+      table_name,
+      concurrently: true,
+      cascade: false
+    )
+  end
+
+  def readonly?
+    true
+  end
 end

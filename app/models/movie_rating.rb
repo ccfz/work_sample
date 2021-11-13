2148b463db42a7888e1575a716dcd7e5c9561e1b
ccfz <casparfz@gmail.com>
2021-11-13 22:29:45 +0100

    Add unique primary key to MovieRating View
    In anticipation of users adding more ratings the materialized view
    needs to be updatable, without getting locked. This is only possible
    if the table has a unique index.
    
    This makes the MovieRating id (movie id) the primary unique key.

--- app/models/movie_rating.rb
+++ app/models/movie_rating.rb
@@ -1,2 +1,5 @@
 class MovieRating < ApplicationRecord
+  belongs_to :movie
+  self.primary_key= :id
+
 end
--- db/migrate/20211113172200_create_movie_ratings.rb
+++ db/migrate/20211113172200_create_movie_ratings.rb
@@ -1,5 +1,6 @@
 class CreateMovieRatings < ActiveRecord::Migration[6.0]
   def change
     create_view :movie_ratings, materialized: true
+    add_index :movie_ratings, :id, unique: true
   end
 end
